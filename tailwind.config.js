afd1469090c525b7064633ca88d80e9b68677e22
Mattias <99132374+lukasmattias@users.noreply.github.com>
2024-03-29 14:10:45 -0300

    header + habilidades-section
    -header com hambúrguer funcional (ainda com erros);
    -habilidades-section responsivo;

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
